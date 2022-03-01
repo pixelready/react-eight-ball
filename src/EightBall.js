63af9c8930f3489bc8ac680db743959f1baea71c
pixelready <bizartist@gmail.com>
2022-03-01 12:21:26 -0800

    code review tweaks

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,10 +1,14 @@
 import React, {useState} from "react";
 import './EightBall.css';
 
+/**
+ * 
+ * 
+ */
 function EightBall({answers}){
     const [msg, setMsg] = useState("Think of a question");
-    const [color, setColor] = useState("green");
-    let eightBallBackground = {backgroundColor: color};
+    const [color, setColor] = useState("black");
+    
     function _getRandomAnswer(){
         const randAnswerIdx = Math.floor(Math.random() * answers.length);
         return answers[randAnswerIdx];
@@ -14,11 +18,17 @@ function EightBall({answers}){
         const {msg,color} = _getRandomAnswer();
         setMsg(msg);
         setColor(color);
-        eightBallBackground = {backgroundColor: color};
     }
 
+    let eightBallBackground = {backgroundColor: color};
     return (
-        <button onClick = {handleClick} className="eightball-button" style={eightBallBackground}>{msg}</button>
+        <button 
+            onClick={handleClick} 
+            className="eightball-button" 
+            style={eightBallBackground}
+        >
+            {msg}
+        </button>
     );
     
 
